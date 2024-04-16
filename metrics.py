e5c095ae1c6e3cdd2a0a1e05966c4edda7ee1a3f
sanyuktaadap <sanyuadap@gmail.com>
2024-04-16 18:29:43 -0400

    Minor changes

--- metrics.py
+++ metrics.py
@@ -2,19 +2,19 @@ import torchmetrics as tm
 
 def get_metrics(y_hat, y, metric=None):
     if metric == "accuracy":
-        fn = tm.Accuracy(task="multiclass", num_classes=4)
+        fn = tm.Accuracy(task="multiclass", num_classes=8)
         score = fn(y_hat, y)
     elif metric == "specificity":
-        fn = tm.classification.MulticlassSpecificity(num_classes=4, average=None)
+        fn = tm.classification.MulticlassSpecificity(num_classes=8, average=None)
         score = fn(y_hat, y)
     elif metric == "precision":
-        fn = tm.classification.MulticlassPrecision(num_classes=4, average=None)
+        fn = tm.classification.MulticlassPrecision(num_classes=8, average=None)
         score = fn(y_hat, y)
     elif metric == "recall":
-        fn = tm.classification.MulticlassRecall(num_classes=4, average=None)
+        fn = tm.classification.MulticlassRecall(num_classes=8, average=None)
         score = fn(y_hat, y)
     elif metric == "f1":
-        fn = tm.classification.MulticlassF1Score(num_classes=4, average=None)
+        fn = tm.classification.MulticlassF1Score(num_classes=8, average=None)
         score = fn(y_hat, y)
 
     return score
